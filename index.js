3de53a865fecaeb9daa4a179e5ba5b3117ee4d1d
DevTec2020 <leonardo.silva.inf@gmail.com>
2024-09-10 21:11:23 -0300

    Implementando os primeiros metodos como add tarefas e marcar como feitas

---
+++ index.js
@@ -0,0 +1,87 @@
+const { select, input, checkbox } = require('@inquirer/prompts');
+
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+
+let metas = [meta]
+
+const cadastrarMeta = async () => {
+    const meta = await input ({message: "Digite a meta "})
+
+    if (meta.length == 0){
+        console.log("A meta não pode ser vazia")
+        return
+    }
+
+    metas.push(
+        {value: meta, checked: false}
+    )
+        
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox ({
+        message:"\nObs. use as setas para mudar de meta, espaço para marcar e desmarcar, enter para finalizar essa etapa.\nMetas:",
+        message:"\nObs. use as setas para mudar de meta, espaço para marcar e desmarcar, enter para finalizar essa etapa.\nMetas:",
+        choices:[...metas],
+        //Desabilita as instruções por que vem em incles e la em message eu expliquei como usar
+        instructions: false,
+    })
+
+    if (respostas.length == 0){
+        console.log("\nNenhuma meta selecionada\n")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log("\nMeta(s) marcadas como concluida(s)\n")
+}
+
+
+const start = async () => {
+    while (true) {
+        //Codigo para aguarar o usuário digitar algo antes de proceguir
+        const opcao = await select({
+            message: "Menu",
+            choices: [
+                { name: "Cadastrar meta", value: "cadastrar" },
+                { name: "Listar metas", value: "listar"},
+                { name: "Sair", value: "sair" }
+            ]
+        });
+
+        switch (opcao) {
+            case "cadastrar":
+                await cadastrarMeta();
+                console.log(metas)
+                break;
+            
+            case "listar":
+                await listarMetas();
+                break;
+
+            case "sair":
+                console.log("\nSaindo...\n");
+                return; // Sai da função, encerrando o loop
+
+            default:
+                console.log("Opção inválida");
+        }
+    }
+};
+
+// Chama a função para iniciar o menu
+start();
