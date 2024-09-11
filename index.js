8e5de16a81774eea7ff2b3fcde41a74ca75b67c0
DevTec2020 <leonardo.silva.inf@gmail.com>
2024-09-11 08:51:36 -0300

    Corrigindo bug de marcar e desmarcar

--- index.js
+++ index.js
@@ -30,14 +30,14 @@ const listarMetas = async () => {
         instructions: false,
     })
 
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
     if (respostas.length == 0){
         console.log("\nNenhuma meta selecionada\n")
         return
     }
-
-    metas.forEach((m) => {
-        m.checked = false
-    })
 
     respostas.forEach((resposta) => {
         const meta = metas.find((m) => {
